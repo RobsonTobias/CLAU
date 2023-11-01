548569b37b74300b63e851bec50ba3267663567c
Robson Tobias <robsontsiqueir@gmail.com>
2023-10-31 21:51:21 -0300

    Alteração do Menu-Dropdown
    Adicionado hover na imagem e busca o nome do usuário.

--- SITE/PHP/dropdown.php
+++ SITE/PHP/dropdown.php
@@ -8,7 +8,8 @@ if (isset($_SESSION['Usuario']) && !empty($_SESSION['Usuario'])) {
     $dropdown ='
     <p>' . $Nome . '</p>
     <div class="dropdown">
-        <img src="https://placekitten.com/400/400" alt="Perfil" onclick="myFunction()" class="dropbtn">
+        <img src="https://placekitten.com/400/400" alt="Perfil" onclick="myFunction()"   class="dropbtn">
+        <div style="height:70px;width:70px; position:absolute" onclick="myFunction()" class="dropbtn hover"></div>
         <div id="myDropdown" class="dropdown-content">
             <a href="perfil.php">Perfil</a>
             <a href="notificacoes.php">Notificação</a>
