ef081e25f0eee2a15fbcd5b322a24fcc7785c1d6
Robson Tobias <robsontsiqueir@gmail.com>
2024-05-11 21:23:11 -0300

    Alterado Layout
    alterado o css da pagina de consulta de curso.
    alterado item no menu.css devido o bootstrap.

--- SITE/PAGES/s_curso_consulta.php
+++ SITE/PAGES/s_curso_consulta.php
@@ -1,3 +1,11 @@
+<?php
+include ('../conexao.php');
+
+if (session_status() == PHP_SESSION_NONE) {
+    // Se não houver sessão ativa, inicia a sessão
+    session_start();
+}
+?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 
@@ -5,27 +13,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CLAU - Sistema de Gestão Escolar</title>
-    <link rel="stylesheet" href="../PHP/sidebar/menu.css">
-    <link rel="stylesheet" href="../STYLE/botao.css" />
-    <link rel="stylesheet" href="../STYLE/data.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
         integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="../PHP/sidebar/menu.css">
+    <link rel="stylesheet" href="../STYLE/botao.css" />
+    <link rel="stylesheet" href="../STYLE/data.css">
+
     <link rel="stylesheet" href="../STYLE/style_home.css">
     <link rel="icon" href="../ICON/C.svg" type="image/svg">
     <style>
         .curso path {
             fill: #043140;
         }
-        .top{
+
+        .top {
             position: relative;
             display: flex;
             width: 100%;
-            justify-content:center;
+            justify-content: center;
             align-items: flex-end;
         }
-        .top a{
+
+        .top a {
             text-decoration: none;
             color: #4CAF50;
             position: absolute;
@@ -34,15 +47,16 @@
             display: flex;
             transition: 0.2s ease-in-out;
         }
-        .top a:hover{
+
+        .top a:hover {
             opacity: 0.5;
         }
 
-        .top a img{
+        .top a img {
             margin-right: 3px;
         }
 
-        
+
         table {
             border-collapse: collapse;
             width: 80%;
@@ -50,11 +64,11 @@
             /* Centraliza a tabela */
             border-radius: 15px;
         }
-        
+
         th {
             background-color: #f2f2f2;
             color: #000;
-            
+
             /* ou a cor desejada para o texto do cabeçalho */
         }
 
@@ -82,22 +96,78 @@
         td {
             padding: 8px;
             text-align: center;
-            color:#233939
-            /* Centraliza o texto */
+            color: #233939
+                /* Centraliza o texto */
         }
 
         /* Removendo as bordas internas das células */
         th,
         td {
             border-bottom: 1px solid #ddd;
-            
+
             /* Linha sutil no fundo de cada célula */
         }
 
-        .titulos th{
+        .titulos th {
             background-color: #2E2E2E;
             color: white;
             cursor: default;
+        }
+
+
+        main {
+            margin: 0;
+            padding: 0;
+            margin-top: 2%;
+        }
+
+        .principal {
+            /* width: 20rem; */
+            background-color: #E7E7E7;
+            border-radius: 1.25rem;
+            border: none;
+            box-shadow: 0 0 0.313rem 0.063rem #00000040;
+            padding: 1.25rem;
+        }
+
+        .card-title {
+            font-size: 1.375em;
+            font-weight: bold;
+            color: #233939;
+            margin: 0;
+        }
+
+        p {
+            margin: 0;
+        }
+
+        .row {
+            margin: 0;
+        }
+
+        .adicionar {
+            border-radius: 50%;
+            height: 1.25rem;
+            width: 1.25rem;
+            font-size: 1em;
+            font-weight: bolder;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: #4CAF50;
+            border: none;
+            color: #FFFFFF;
+        }
+
+        a p {
+            color: #4CAF50;
+            font-weight: bolder;
+            margin-left: 0.5em;
+        }
+
+        a:hover {
+            text-decoration: none;
+            opacity: 0.5;
         }
     </style>
 
@@ -105,10 +175,10 @@
 
 <body>
 
-    <?php include('../PHP/data.php'); ?>
-    <?php include('../PHP/sidebar/menu.php'); ?>
-    <?php include('../PHP/redes.php'); ?>
-    <?php include('../PHP/dropdown.php'); ?>
+    <?php include ('../PHP/data.php'); ?>
+    <?php include ('../PHP/sidebar/menu.php'); ?>
+    <?php include ('../PHP/redes.php'); ?>
+    <?php include ('../PHP/dropdown.php'); ?>
 
     <header>
         <div class="title">
@@ -131,13 +201,62 @@
     </div>
 
     <main>
+        <div class="card row principal">
+            <div class="row justify-content-between">
+                <p class="card-title">Lista de Cursos</p>
+                <a href="s_curso_cad.php" class="row d-flex align-items-center">
+                    <button class="adicionar" type="button">+</button>
+                    <p>Adicionar Curso</p>
+                </a>
+            </div>
+            <div class="table-responsive">
+                <table class="table table-hover">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th>Curso</th>
+                            <th class="text-center">Sigla</th>
+                            <th class="text-center">Duração</th>
+                            <th class="text-center">Carga Horária</th>
+                            <th class="text-center">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $sql = "SELECT * FROM curso";
+                        $resultado = $conn->query($sql);
+                        if ($resultado && $resultado->num_rows > 0) {
+                            while ($row = $resultado->fetch_assoc()) {
+                                ?>
+                                <tr data-id='"<?php $row['$curso_id']; ?>"'>
+                                    <td class="text-left"><?php echo $row['Curso_Nome']; ?></td>
+                                    <td><?php echo $row['Curso_Sigla']; ?></td>
+                                    <td><?php echo $row['Curso_Duracao']; ?> meses</td>
+                                    <td><?php echo $row['Curso_Carga_horaria']; ?> horas</td>
+                                    <td><?php echo ($row['Curso_Status']== 1 ? "Ativo" : "Inativo"); ?></td>
+                                </tr>
+                                <?php
+                            }
+                        } else {
+                            echo "<tr><td colspan='2'>Nenhum curso encontrado.</td></tr>";
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
 
-<div class="top"><h1>Lista de Cursos</h1>
-        <a href="s_curso_cad.php"><img src="../ICON/sinal-mais.svg" alt="">Adicionar Curso</a></div>
-        
+
+
+
+
+        <div class="top">
+            <h1>Lista de Cursos</h1>
+            <a href="s_curso_cad.php"><img src="../ICON/sinal-mais.svg" alt="">Adicionar Curso</a>
+        </div>
+
         <table>
             <tr class="titulos">
-                
+
                 <th>ID do Curso</th>
                 <th>Nome do Curso</th>
                 <th>Sigla</th>
@@ -146,7 +265,7 @@
                 <th>Duração</th>
                 <th>Pré-requisito</th>
                 <th>Status</th>
-                
+
             </tr>
             <?php
             $servername = "localhost";
@@ -191,6 +310,15 @@
     <script src="../JS/dropdown.js"></script>
     <script src="../JS/botao.js"></script>
     <script src="../PHP/sidebar/menu.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"
+        integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
