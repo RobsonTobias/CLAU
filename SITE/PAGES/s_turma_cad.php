729a77f03f0656fd3bc876f6df8ccf2e60a0513a
Vinícius Gonçalves <70039646+vg1802@users.noreply.github.com>
2024-06-23 16:54:55 -0300

    alteracao no layout da tela de cadastro de turmas

--- SITE/PAGES/s_turma_cad.php
+++ SITE/PAGES/s_turma_cad.php
@@ -25,40 +25,62 @@ $titulo = 'CADASTRO DE TURMAS'; //Título da página, que fica sobre a data
     <link rel="icon" href="../ICON/C.svg" type="image/svg">
 
     <style>
-    input[type=text],
-    input[type=number],
-    input[type=time],
-    input[type=date],
-    select,
-    textarea {
-        background-color: #949494;
-        color: white;
-    }
+        @media (max-width: 300px) {
+            .card-body {
+                padding: 0;
+            }
+        }
 
-    input[type=text]:focus,
-    input[type=number]:focus,
-    input[type=time]:focus,
-    input[type=date]:focus,
-    select:focus,
-    textarea:focus {
-        background-color: #949494;
-        color: white;
-        border: black;
-        box-shadow: none;
-    }
+        @media (max-width: 1000px) {
+            .card {
+                margin-left: 80px;
+            }
+        }
 
-    .form-control:focus {
-        box-shadow: none;
-    }
+        main {
+            padding: 0;
+        }
 
-    .card-body {
-        background: #E7E7E7;
-    }
+        input[type=text],
+        input[type=number],
+        input[type=time],
+        input[type=date],
+        select,
+        textarea {
+            background-color: #949494;
+            color: white;
+        }
 
-    .rounded-pill {
-        border-radius: 50px !important;
-    }
-</style>
+        input[type=text]:focus,
+        input[type=number]:focus,
+        input[type=time]:focus,
+        input[type=date]:focus,
+        select:focus,
+        textarea:focus {
+            background-color: #949494;
+            color: white;
+            border: black;
+            box-shadow: none;
+        }
+
+        .form-control:focus {
+            box-shadow: none;
+        }
+
+        .card {
+            padding: 0;
+            background: #E7E7E7;
+        }
+
+        .rounded-pill {
+            border-radius: 50px !important;
+        }
+
+        #Turma_Cod {
+            background-color: #949494;
+            color: white;
+        }
+    </style>
 
 </head>
 
@@ -70,125 +92,125 @@ $titulo = 'CADASTRO DE TURMAS'; //Título da página, que fica sobre a data
 
 
     <main class="container-fluid mt-4">
-    <div class="card col-sm-12">
-        <div class="card-header">
-            <h2>Cadastro de Turmas</h2>
-        </div>
-        <div class="card-body">
-            <form action="../PHP/cad_turma.php" method="post" id="formDias">
-                <div class="form-group">
-                    <label for="Turma_Cod">Código da Turma:</label>
-                    <input type="text" class="form-control rounded-pill" id="Turma_Cod" name="Turma_Cod" readonly required>
-                </div>
+        <div class="card col-sm-12">
+            <div class="card-header">
+                <h2 class="text-center">Cadastro de Turmas</h2>
+            </div>
+            <div class="card-body">
+                <form action="../PHP/cad_turma.php" method="post" id="formDias">
+                    <div class="form-group">
+                        <label for="Turma_Cod">Código da Turma:</label>
+                        <input type="text" class="form-control rounded-pill" id="Turma_Cod" name="Turma_Cod" readonly required>
+                    </div>
 
-                <div class="form-group">
-                    <label for="Curso_id">Curso</label>
-                    <select class="form-control rounded-pill" id="Curso_id" name="Curso_id" onchange="atualizarCodigoTurma()">
-                        <?php
-                        include '../conexao.php';
+                    <div class="form-group">
+                        <label for="Curso_id">Curso</label>
+                        <select class="form-control rounded-pill" id="Curso_id" name="Curso_id" onchange="atualizarCodigoTurma()">
+                            <?php
+                            include '../conexao.php';
 
-                        if ($conn->connect_error) {
-                            die("Conexão falhou: " . $conn->connect_error);
-                        }
-
-                        $sql = "SELECT Curso_id, Curso_Nome, Curso_Sigla FROM Curso";
-                        $result = $conn->query($sql);
-
-                        if ($result->num_rows > 0) {
-                            while ($row = $result->fetch_assoc()) {
-                                echo "<option value='" . $row["Curso_Sigla"] . "'>" . $row["Curso_Nome"] . "</option>";
+                            if ($conn->connect_error) {
+                                die("Conexão falhou: " . $conn->connect_error);
                             }
-                        } else {
-                            echo "<option>Nenhum curso encontrado</option>";
-                        }
 
-                        $conn->close();
-                        ?>
-                    </select>
-                </div>
+                            $sql = "SELECT Curso_id, Curso_Nome, Curso_Sigla FROM Curso";
+                            $result = $conn->query($sql);
 
-                <div class="form-group">
-                    <label for="Turma_Horario_inicio">Horário de início:</label>
-                    <input type="time" class="form-control rounded-pill" min="08:00" max="18:00" id="Turma_Horario_inicio" name="Turma_Horario_inicio" required>
-                </div>
-
-                <div class="form-group">
-                    <label for="Turma_Horario_termino">Horário de término:</label>
-                    <input type="time" class="form-control rounded-pill" min="10:00" max="20:00" id="Turma_Horario_termino" name="Turma_Horario_termino" required>
-                </div>
-
-                <div class="form-group">
-                    <label for="Turma_Vagas">Vagas:</label>
-                    <input type="number" class="form-control rounded-pill" id="Turma_Vagas" name="Turma_Vagas" required>
-                </div>
-
-                <div class="form-group">
-                    <label for="Turma_Dias">Dias:</label><br>
-                    <?php
-                    $dias_semana = array(
-                        1 => 'Segunda-feira',
-                        2 => 'Terça-feira',
-                        3 => 'Quarta-feira',
-                        4 => 'Quinta-feira',
-                        5 => 'Sexta-feira',
-                        6 => 'Sábado'
-                    );
-
-                    foreach ($dias_semana as $id_dia => $nome_dia) {
-                        echo "<div class='form-check'>
-                                <input class='form-check-input' type='checkbox' name='Turma_Dias[]' value='$id_dia' id='dia_$id_dia'>
-                                <label class='form-check-label' for='dia_$id_dia'>$nome_dia</label>
-                              </div>";
-                    }
-                    ?>
-                    <input type="hidden" id="codigo_dias" name="codigo_dias" readonly>
-                </div>
-
-                <div class="form-group">
-                    <label for="Turma_Obs">Observações:</label>
-                    <textarea class="form-control rounded-pill" id="Turma_Obs" name="Turma_Obs" required></textarea>
-                </div>
-
-                <div class="form-group">
-                    <label for="Turma_Inicio">Data de Início:</label>
-                    <input type="date" class="form-control rounded-pill" id="Turma_Inicio" name="Turma_Inicio" required>
-                </div>
-
-                <div class="form-group">
-                    <label for="Turma_Termino">Data de Término:</label>
-                    <input type="date" class="form-control rounded-pill" id="Turma_Termino" name="Turma_Termino" required>
-                </div>
-
-                <div class="form-group">
-                    <label for="Professor_id">Professor:</label>
-                    <select class="form-control rounded-pill" id="Professor_id" name="Professor_id">
-                        <?php
-                        include '../conexao.php';
-
-                        $sql = "SELECT U.Usuario_id, U.Usuario_Nome FROM Usuario U
-                                INNER JOIN Registro_Usuario RU ON U.Usuario_id = RU.Usuario_Usuario_cd
-                                INNER JOIN Tipo T ON RU.Tipo_Tipo_cd = T.Tipo_id
-                                WHERE T.Tipo_id = 4";
-                        $result = $conn->query($sql);
-
-                        if ($result->num_rows > 0) {
-                            while ($row = $result->fetch_assoc()) {
-                                echo "<option value='" . $row["Usuario_id"] . "'>" . $row["Usuario_Nome"] . "</option>";
+                            if ($result->num_rows > 0) {
+                                while ($row = $result->fetch_assoc()) {
+                                    echo "<option value='" . $row["Curso_Sigla"] . "'>" . $row["Curso_Nome"] . "</option>";
+                                }
+                            } else {
+                                echo "<option>Nenhum curso encontrado</option>";
                             }
-                        } else {
-                            echo "<option value=''>Nenhum professor encontrado</option>";
+
+                            $conn->close();
+                            ?>
+                        </select>
+                    </div>
+                    <div class="form-group d-flex flex-wrap">
+                        <div class="form-group">
+                            <label for="Turma_Horario_inicio">Horário de início:</label>
+                            <input type="time" class="form-control rounded-pill" min="08:00" max="18:00" id="Turma_Horario_inicio" name="Turma_Horario_inicio" required>
+                        </div>
+
+                        <div class="form-group pl-2">
+                            <label for="Turma_Horario_termino">Horário de término:</label>
+                            <input type="time" class="form-control rounded-pill" min="10:00" max="20:00" id="Turma_Horario_termino" name="Turma_Horario_termino" required>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="Turma_Vagas">Vagas:</label>
+                        <input type="number" class="form-control rounded-pill" id="Turma_Vagas" name="Turma_Vagas" required>
+                    </div>
+
+                    <div class="form-group d-flex flex-wrap">
+                        <label for="Turma_Dias">Dias:</label><br>
+                        <?php
+                        $dias_semana = array(
+                            1 => 'Segunda-feira',
+                            2 => 'Terça-feira',
+                            3 => 'Quarta-feira',
+                            4 => 'Quinta-feira',
+                            5 => 'Sexta-feira',
+                            6 => 'Sábado'
+                        );
+
+                        foreach ($dias_semana as $id_dia => $nome_dia) {
+                            echo "<div class='form-check pl-5'>
+                                    <input class='form-check-input' type='checkbox' name='Turma_Dias[]' value='$id_dia' id='dia_$id_dia'>
+                                    <label class='form-check-label' for='dia_$id_dia'>$nome_dia</label>
+                                  </div>";
                         }
-
-                        $conn->close();
                         ?>
-                    </select>
-                </div>
+                        <input type="hidden" id="codigo_dias" name="codigo_dias" readonly>
+                    </div>
 
-                <button type="submit" class="btn btn-primary rounded-pill">Enviar</button>
-            </form>
+                    <div class="form-group">
+                        <label for="Turma_Obs">Observações:</label>
+                        <textarea class="form-control" id="Turma_Obs" name="Turma_Obs" required></textarea>
+                    </div>
+                    <div class="form-group d-flex flex-wrap">
+                        <div class="form-group">
+                            <label for="Turma_Inicio">Data de Início:</label>
+                            <input type="date" class="form-control rounded-pill" id="Turma_Inicio" name="Turma_Inicio" required>
+                        </div>
+
+                        <div class="form-group pl-2">
+                            <label for="Turma_Termino">Data de Término:</label>
+                            <input type="date" class="form-control rounded-pill" id="Turma_Termino" name="Turma_Termino" required>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="Professor_id">Professor:</label>
+                        <select class="form-control rounded-pill" id="Professor_id" name="Professor_id">
+                            <?php
+                            include '../conexao.php';
+
+                            $sql = "SELECT U.Usuario_id, U.Usuario_Nome FROM Usuario U
+                                    INNER JOIN Registro_Usuario RU ON U.Usuario_id = RU.Usuario_Usuario_cd
+                                    INNER JOIN Tipo T ON RU.Tipo_Tipo_cd = T.Tipo_id
+                                    WHERE T.Tipo_id = 4";
+                            $result = $conn->query($sql);
+
+                            if ($result->num_rows > 0) {
+                                while ($row = $result->fetch_assoc()) {
+                                    echo "<option value='" . $row["Usuario_id"] . "'>" . $row["Usuario_Nome"] . "</option>";
+                                }
+                            } else {
+                                echo "<option value=''>Nenhum professor encontrado</option>";
+                            }
+
+                            $conn->close();
+                            ?>
+                        </select>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary rounded-pill">Enviar</button>
+                </form>
+            </div>
         </div>
-    </div>
-</main>
+    </main>
 
 
     <div class="buttons">
